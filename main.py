65b072eab06c10875f6c950e0c89891ddc6200ec
ZynoZin <zineddinelamziouik@gmail.com>
2021-06-06 14:02:34 +0100

    working prototype

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import cv2
 from tensorflow import keras
 import numpy as np
 import imutils
+from serial import Serial
+import time
+serialcomm = Serial('COM6', 9600)
+serialcomm.timeout = 1
 model = keras.models.load_model('./HGR_CNN.h5')
 camera = cv2.VideoCapture(0)
 dim = (100, 100)
@@ -18,7 +22,7 @@ while(True):
     (startX, startY, endX, endY) = box.astype("int")
     cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 1)
     cv2.imshow('img', img)
-    if(frame_count % 15 == 0):
+    if(frame_count % 20 == 0):
         crop_img = img[50:350, 50:350]
         resized = crop_img
         resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
@@ -29,18 +33,12 @@ while(True):
         # 0= backward - 1 = forward - 2 = left - 3 = right - 4 = stop
         if(prediction_c != previous_prediction_c):
             previous_prediction_c = prediction_c
-            if prediction_c == 0:
-                print("backward")
-            elif prediction_c == 1:
-                print("forward")
-            elif prediction_c == 3:
-                print("left")
-            elif prediction_c == 2:
-                print("right")
-            elif prediction_c == 4:
-                print("stop")
+            i = f"{prediction_c[0]}"
+            serialcomm.write(i.encode())
+            time.sleep(0.5)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 vid.release()
+serialcomm.close()
 cv2.destroyAllWindows()
